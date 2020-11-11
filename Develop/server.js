4b7b72413ce8d2e11844ebc47225d87d7bf04b69
Vic Scherman <vicscherman@gmail.com>
2020-11-10 20:43:46 -0500

    deploying

--- Develop/server.js
+++ Develop/server.js
@@ -18,13 +18,14 @@ app.use(express.static("public"));
 // connection.once('open', () => {
 //   console.log("MongoDB database connection established successfully");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', 
 { 
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
- });
+ }
+ );
 
 require("./models");
 
